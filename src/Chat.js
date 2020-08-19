304d8cf43713fc7066d3bb2d3bad803e61d6b7eb
Rebraxin <cedric.paje@gmail.com>
2020-08-19 21:30:43 +0200

    getting rdy to deploy

--- src/Chat.js
+++ src/Chat.js
@@ -61,7 +61,12 @@ function Chat() {
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
         <div className="chat__headerInfo">
           <h3>{roomName}</h3>
-          <p>Last seen at...</p>
+          <p>
+            Last seen{' '}
+            {new Date(
+              messages[messages.length - 1]?.timestamp?.toDate()
+            ).toUTCString()}
+          </p>
         </div>
         <div className="chat__headerRight">
           <IconButton>
@@ -77,7 +82,11 @@ function Chat() {
       </div>
       <div className="chat__body">
         {messages.map((message) => (
-          <p className={`chat__message ${message.name === user.DisplayName && 'chat__reciever'}`}>
+          <p
+            className={`chat__message ${
+              message.name === user.DisplayName && 'chat__reciever'
+            }`}
+          >
             <span className="chat__name">{message.name}</span>
             {message.message}
             <span className="chat__timestamp">
